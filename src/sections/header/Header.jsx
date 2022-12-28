70884a083363b64e7a3f32a9245e2eeb7c7e1023
Amir Sohail <amirsohail335@gmail.com>
2022-12-28 11:26:47 +0530

    committed

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,23 @@
-import './header.css'
+import "./header.css";
+import HeaderImage from "../../assets/header.jpg";
 
 const Header = () => {
   return (
-    <header id="header"><h2>HEader</h2></header>
-  )
-}
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portait" />
+        </div>
+        <h3>Amir Sohail</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+          itaque dicta et odit. Quaerat atque modi, enim distinctio commodi
+          amet, porro, accusamus ut cupiditate nihil numquam dolor obcaecati
+          fugiat alias inventore
+        </p>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
