27765cf2caf41c1c740c32871991cf84ef260860
Amir Sohail <amirsohail335@gmail.com>
2023-01-20 23:31:13 +0530

    committed

--- src/theme/data.js
+++ src/theme/data.js
@@ -4,7 +4,7 @@ export const primaryColors = [
     {className: 'color-3'},
     {className: 'color-4'},
     {className: 'color-5'},
-    // {className: 'color-6'}
+    {className: 'color-6'}
 ]
 
 export const backgroundColors = [
